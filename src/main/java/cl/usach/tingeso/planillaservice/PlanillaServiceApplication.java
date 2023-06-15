3880d5284831a3c4794e37360f14277b07aff72f
Jovanni Schneider <jovanni.schneider@usach.cl>
2023-06-15 02:00:31 -0400

    [HU1 y HU2]

---
+++ src/main/java/cl/usach/tingeso/planillaservice/PlanillaServiceApplication.java
@@ -0,0 +1,13 @@
+package cl.usach.tingeso.planillaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanillaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanillaServiceApplication.class, args);
+    }
+
+}
